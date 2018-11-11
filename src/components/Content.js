6d5292c45d69cd66829f4430d6ef3789ae5c8599
Mario Aranda <mario.aranda2551@gmail.com>
2018-11-10 19:40:08 -0600

    additions made

--- src/components/Content.js
+++ src/components/Content.js
@@ -4,16 +4,21 @@ import List from './List';
 import * as LocationsAPI from '../api/Locations';
 
 class Content extends React.Component {
-  componentDidMount (){
-    LocationsAPI.getLocations().then => console.log("resp", resp);
+  state = {
+    locations: []
+  };
+  componentDidMount() {
+    console.log("data");
+    LocationsAPI.getLocations().then(resp => this.setState({ locations: resp })
+    );
   }
-
   render() {
-    console.log("RESPONSE");
+    console.log("this.state.locations");
+
     return (
       <div className="content">
-        <List />
-        <Map />
+        <List locations={this.state.locations} />
+        <Map locations={this.state.locations} />
       </div>
     );
   }
--- src/components/List.js
+++ src/components/List.js
@@ -3,10 +3,22 @@ import Map from './Map';
 
 class List extends React.Component {
   render() {
-      // lists of locations;
+
+    const locations = this.props.locations;
     return (
       <div id="list">
-        <h2>List</h2>
+        <h2>Locations</h2>
+        <input type="text" />
+        <ol>
+          {locations.map(loc => (
+            <li key={loc.venue.id}>
+              <div>
+                <h2>Name: {loc.venue.name}</h2>
+                <p>Address: {loc.venue.location.address}</p>
+              </div>
+            </li>
+          ))}
+        </ol>
       </div>
     );
   }
