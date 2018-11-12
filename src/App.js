6f13ee3967e50088655bee42e6c8418c2c1eef23
Mario Aranda <mario.aranda2551@gmail.com>
2018-11-11 20:53:05 -0600

    Final Changes

--- src/App.js
+++ src/App.js
@@ -3,12 +3,99 @@ import './App.css';
 import Header from './components/Header';
 import Content from './components/Content';
 
+// style from Snazzy Maps
+// Simple night vision - Stranger Thing
+// by beniamino nobile
+var styles = [
+  {
+        featureType: 'all',
+        elementType: 'all',
+        stylers: [
+            {
+                invert_lightness: true
+            },
+            {
+                saturation: '-9'
+            },
+            {
+                lightness: '0'
+            },
+            {
+                visibility: 'simplified'
+            }
+        ]
+    },
+    {
+        featureType: 'landscape.man_made',
+        elementType: 'all',
+        stylers: [
+            {
+                weight: '1.00'
+            }
+        ]
+    },
+    {
+        featureType: 'road.highway',
+        elementType: 'all',
+        stylers: [
+            {
+                weight: '0.49'
+            }
+        ]
+    },
+    {
+        featureType: 'road.highway',
+        elementType: 'labels',
+        stylers: [
+            {
+                visibility: 'on'
+            },
+            {
+                weight: '0.01'
+            },
+            {
+                lightness: '-7'
+            },
+            {
+                saturation: '-35'
+            }
+        ]
+    },
+    {
+        featureType: 'road.highway',
+        elementType: 'labels.text',
+        stylers: [
+            {
+                visibility: 'on'
+            }
+        ]
+    },
+    {
+        featureType: 'road.highway',
+        elementType: 'labels.text.stroke',
+        stylers: [
+            {
+                visibility: 'off'
+            }
+        ]
+    },
+    {
+        featureType: 'road.highway',
+        elementType: 'labels.icon',
+        stylers: [
+            {
+                visibility: 'on'
+            }
+        ]
+    }
+];
 
 class App extends Component {
   initMap() {
     let map = new window.google.maps.Map(document.getElementById('map'), {
       center: {lat: 41.8781, lng: -87.6298},
-      zoom: 12
+      zoom: 12,
+      styles: styles
     });
     window.mapObject = map;
   }
@@ -34,7 +121,7 @@ class App extends Component {
   render() {
     this.loadScript();
     return (
-      <div className="App">
+      <div className='App'>
         <Header />
         <Content />
       </div>
